6e5077abdb3c3bfe3297608152b45c4d3d0fd3bf
Hasnat alam <hasnatalamofficial@gmail.com>
2025-03-03 11:01:55 +0530

    minor change

--- SpringBootProjectLayredApp-3(CRUDWithJDBC)/src/main/java/com/hasnat/dao/OracleEmployeeDAO.java
+++ SpringBootProjectLayredApp-3(CRUDWithJDBC)/src/main/java/com/hasnat/dao/OracleEmployeeDAO.java
@@ -20,49 +20,50 @@ public class OracleEmployeeDAO implements IEmployeeDAO {
 	private DataSource ds;
 
 //SQL Query
-	private static final String GET_EMPS_QUERY ="SELECT EMPNO, ENAME, DESG, SALARY, DEPTNO FROM EMPLOYEE WHERE DESG IN (?, ?, ?)";
+	private static final String GET_EMPS_QUERY = "SELECT EMPNO, ENAME, DESG, SALARY, DEPTNO FROM EMPLOYEE WHERE DESG IN (?, ?, ?)";
 	private static final String INSERT_EMP_QUERY = "INSERT INTO Employee (EMPNO, ENAME, DESG, SALARY, DEPTNO)VALUES (EMP_SEQ.NEXTVAL,?, ?, ?, ?)";
-	 
-	
 
-	
 	@Override
 	public List<Employee> showEmployeeByDesg(String desg1, String desg2, String desg3) throws Exception {
-		List<Employee> list=null;
-		//get pooled jdbc con
-		//get PreparedStatement object with pre-compiled SQL Query
-		try(Connection con = ds.getConnection();
-			PreparedStatement ps = con.prepareStatement(GET_EMPS_QUERY);
-		){
-			//Set values to query params 
+		List<Employee> list = null;
+		// get pooled jdbc con
+		// get PreparedStatement object with pre-compiled SQL Query
+		try (Connection con = ds.getConnection(); PreparedStatement ps = con.prepareStatement(GET_EMPS_QUERY);) {
+			// Set values to query params
 			System.out.println("Entry");
-			ps.setString(1,desg1);
+			ps.setString(1, desg1);
 			ps.setString(2, desg2);
 			ps.setString(3, desg3);
-			//execute the SQL Query
-			try(ResultSet rs = ps.executeQuery()){
+			// execute the SQL Query
+			try (ResultSet rs = ps.executeQuery()) {
 				list = new ArrayList<>();
-				//process the ResultSet object to List<Employee object
-				while(rs.next()) {
-					
-					//copy each record into model class obj
+				// process the ResultSet object to List<Employee object
+				while (rs.next()) {
+
+					// copy each record into model class obj
 					Employee emp = new Employee();
 					emp.setEmpno(rs.getInt(1));
 					emp.setEname(rs.getString(2));
 					emp.setDesg(rs.getString(3));
 					emp.setSalary(rs.getDouble(4));
 					emp.setDeptno(rs.getInt(5));
-					//add Employee class object to List Collection 
+					// add Employee class object to List Collection
 					list.add(emp);
-				}//while
-			}//nested try
-			
-		}//outer try 
-	  
-	  catch (SQLException se) { se.printStackTrace(); throw se;
-	  
-	  } catch (Exception e) { e.printStackTrace(); throw e; } return list; }
-	 
+				} // while
+			} // nested try
+
+		} // outer try
+
+		catch (SQLException se) {
+			se.printStackTrace();
+			throw se;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return list;
+	}
 
 	@Override
 	public int insertEmployee(Employee emp) throws Exception {
@@ -75,7 +76,7 @@ public class OracleEmployeeDAO implements IEmployeeDAO {
 			ps.setString(2, emp.getDesg());
 			ps.setDouble(3, emp.getSalary());
 			ps.setInt(4, emp.getDeptno());
-			// Execute the 	SQL query
+			// Execute the SQL query
 			int count = ps.executeUpdate();
 			return count;
 
@@ -85,6 +86,5 @@ public class OracleEmployeeDAO implements IEmployeeDAO {
 			throw e;
 		}
 	}
-
 
 }
