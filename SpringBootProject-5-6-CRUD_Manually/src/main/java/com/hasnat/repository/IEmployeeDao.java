1c651251e0871af460dd09d345e8d485a06c2ae7
Hasnat alam <hasnatalamofficial@gmail.com>
2025-06-23 09:52:32 +0530

    updateing some in repo

---
+++ SpringBootProject-5-6-CRUD_Manually/src/main/java/com/hasnat/repository/IEmployeeDao.java
@@ -0,0 +1,6 @@
+package com.hasnat.repository;
+
+public interface IEmployeeDao {
+	public int saveEmployee();
+
+}
---
+++ SpringBootProject-5-6-CRUD_Manually/src/test/java/com/hasnat/SpringBootCRUDTests.java
@@ -0,0 +1,13 @@
+package com.hasnat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCRUDTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
